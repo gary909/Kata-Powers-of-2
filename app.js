d6d5edcedaf85588673b353d6581706b38eb5369
gary <gary.white@lodgit.com>
2022-03-09 13:14:31 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function powersOfTwo(n){
+    return []
+}
+
+console.log(powersOfTwo(0)); // return [1]
+console.log(powersOfTwo(1)); // return [1, 2]
+console.log(powersOfTwo(4)); // return [1, 2, 4, 8, 16]
